d49b70e6657ac244862a2a1da2652406874ecfe9
Dmitry Bogdanov <dimon8829@gmail.com>
2020-05-14 19:10:27 +0300

    changed help

--- src/test/java/com/dihri/sport/test/demo/StateTest.java
+++ src/test/java/com/dihri/sport/test/demo/StateTest.java
@@ -1,16 +1,12 @@
 package com.dihri.sport.test.demo;
 
-import com.dihri.sport.test.demo.model.State;
 import com.dihri.sport.test.demo.service.StateService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
